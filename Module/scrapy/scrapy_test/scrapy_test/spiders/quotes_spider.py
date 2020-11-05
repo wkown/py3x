8f4005fe4dc17349223d9f5b9146ace43b133a24
walkskyer <walkskyer@qq.com>
2020-11-05 14:17:16 +0800

    添加一个蜘蛛

---
+++ Module/scrapy/scrapy_test/scrapy_test/spiders/quotes_spider.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# command: scrapy crawl quotes
+# pycharm debug config:
+#       Module name:scrapy.cmdline
+#       Parameters: crawl quotes
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'author': quote.xpath('span/small/text()').get(),
+                'text': quote.css('span.text::text').get(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+if __name__ == "__main__":
+    pass
