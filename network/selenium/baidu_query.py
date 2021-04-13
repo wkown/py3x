7f0c9dc37bdd61fdeafe8714368d04e9851270ca
walkskyer <walkskyer@qq.com>
2021-04-13 09:48:20 +0800

    feat(network/selenium):百度搜索示例，调整搜索

--- network/selenium/baidu_query.py
+++ network/selenium/baidu_query.py
@@ -3,16 +3,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+keywords = "红楼梦"
 driver = webdriver.Chrome()
 driver.get("https://www.baidu.com")
 assert "百度一下，你就知道" in driver.title
 elem = driver.find_element_by_name("wd")
-elem.send_keys("python")
+elem.send_keys(keywords)
 elem.send_keys(Keys.RETURN)
 # time.sleep(5) # 此方式不推荐
 try:
     element = WebDriverWait(driver, 10).until(
-        EC.title_contains("python")
+        EC.title_contains(keywords)
     )
     print(driver.page_source)
 finally:
